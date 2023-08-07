1ad64bb69598f03ca8b533734950e96045d9930b
meinradweiss <meinrad.weiss@hotmail.com>
2023-08-07 17:28:46 +0200

    DateFilterAttributeName processing added.

--- sqldb/SDMT_DB/Core/Stored Procedures/GetSetSlicedImportObjectToLoad.sql
+++ sqldb/SDMT_DB/Core/Stored Procedures/GetSetSlicedImportObjectToLoad.sql
@@ -5,6 +5,7 @@
    ,@SourceObject     sysname          = '%'
    ,@SlicedImportObject_Id varchar(64) = '%'
    ,@Mode             VARCHAR(25)      = 'REGULAR'
+   ,@OrderByFactor    INT              = 1
 )
 AS
 BEGIN
@@ -13,6 +14,8 @@ BEGIN
     ,[SourceSystemName] 
 	,[SourceSchema] 
 	,[SourceObject] 
+    ,[LowWaterMark]
+    ,[HighWaterMark]
   FROM  [Core].[SlicedImportObject]
   WHERE [SourceSystemName] = @SourceSystemName
     AND ((@Mode = 'REGULAR'  AND [LastStart] IS NULL)
@@ -22,4 +25,5 @@ BEGIN
     AND COALESCE([SourceObject],'') LIKE @SourceObject 
     AND CONVERT(VARCHAR(64), [SlicedImportObject_Id]) LIKE @SlicedImportObject_Id 
     AND [Active] = 1
+   ORDER BY TRY_CONVERT(INT, [LowWaterMark]) * @OrderByFactor  DESC
 END
